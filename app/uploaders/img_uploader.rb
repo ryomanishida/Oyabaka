44064be01dd87891ec82f0261cfeee0d67403e04
ryomanishida <riyoma5407@gmail.com>
2021-08-27 13:52:29 +0000

    return:本番環境でのアップロード先変更を戻した

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -4,17 +4,12 @@ class ImgUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development?#環境ごとの保存分別
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir#保存先
+  def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
