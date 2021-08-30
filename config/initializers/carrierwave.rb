c00460a0b175eb11d90024c8496cac8cfa8840e7
ryomanishida <riyoma5407@gmail.com>
2021-08-30 08:30:40 +0000

    画像の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
-    config.storage :fog
+
     config.fog_provider = 'fog-aws'
     config.fog_credentials = {
       provider: 'AWS',
