7232317c552c07869e385bad24624e800db6d1f2
ryomanishida <riyoma5407@gmail.com>
2021-08-30 07:43:53 +0000

    画像の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
-# unless Rails.env.development? || Rails.env.test?
-#   CarrierWave.configure do |config|
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: 'AKIA5G5H7SSRNZKYB5W3',
-#       aws_secret_access_key: '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',
-#       region: 'ap-northeast-1'
-#     }
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIA5G5H7SSRNZKYB5W3',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
 
-#     config.fog_directory  = 'oyabakabucket'
-#     config.cache_storage = :fog/aws
-#   end
-# end
+    config.fog_directory  = 'oyabakabucket'
+    config.cache_storage = :fog
+  end
+end
