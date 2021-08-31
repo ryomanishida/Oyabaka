581589c0c4ce321c8de5ac10e481d901e7d9983a
ryomanishida <riyoma5407@gmail.com>
2021-08-31 08:37:46 +0000

    画像の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,12 @@ require 'carrierwave/storage/fog'
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.storage :fog
+    config.fog_provider = 'fog/aws'
     config.fog_directory  = 'oyabakabucket'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIA5G5H7SSRPQYD4LGB',
-      aws_secret_access_key: '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',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
   end
