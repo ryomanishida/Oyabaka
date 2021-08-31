b75c525f5675b630da43180d6bc7178715eb52f5
ryomanishida <riyoma5407@gmail.com>
2021-08-31 13:30:09 +0000

    画像の修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,15 +10,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to search_contents_path
-    else
-      flash[:notice] = @user.errors.full_messages
-      redirect_to new_user_registration_path
-    end
-  end
+  # def create
+  #   super
+  # end
 
   # GET /resource/edit
   # def edit
